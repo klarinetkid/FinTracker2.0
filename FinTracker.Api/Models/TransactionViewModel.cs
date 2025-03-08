7fd20fbb1178a4266a71cd77dad1857ea78798b6
zmozel <zmozel@spotsolutions.com>
2025-03-08 13:52:58 -0500

    fix create cash transaction issue

--- FinTracker.Api/Models/TransactionViewModel.cs
+++ FinTracker.Api/Models/TransactionViewModel.cs
@@ -9,7 +9,7 @@ namespace FinTracker.Api.Models
         public string? Memo { get; set; }
         public int? CategoryId { get; set; }
         public TblCategory? Category { get; set; }
-
+        public bool? IsCashTransaction { get; set; }
 
         public bool? IsAlreadyImported { get; set; }
         public TblMemo? SavedMemo { get; set; }
@@ -23,7 +23,8 @@ namespace FinTracker.Api.Models
                 Date = Date.HasValue ? Date.Value : throw new Exception("Date is required"),
                 Memo = Memo ?? throw new Exception("Memo is required"),
                 Amount = Amount.HasValue ? Amount.Value : throw new Exception("Amount is required"),
-                CategoryId = CategoryId
+                CategoryId = CategoryId,
+                IsCashTransaction = IsCashTransaction.HasValue ? IsCashTransaction.Value : false,
             };
         }
     }
