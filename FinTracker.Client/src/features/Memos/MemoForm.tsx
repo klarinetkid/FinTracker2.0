10a103ff28e25d52d3b0acadac1cf6137e56f316
zmozel <zmozel@spotsolutions.com>
2025-03-09 13:05:14 -0400

    memos

--- FinTracker.Client/src/features/Memos/MemoForm.tsx
+++ FinTracker.Client/src/features/Memos/MemoForm.tsx
@@ -1,82 +1,54 @@
-import { Controller, useForm } from "react-hook-form";
-import Button from "../../components/Button";
-import ButtonFill from "../../components/ButtonFill";
+import { Controller } from "react-hook-form";
 import CategorySelector from "../../components/CategorySelector";
+import { EntityManagementFormProps } from "../../components/EntityManagementPage";
 import FormGroup from "../../components/FormGroup";
 import Input from "../../components/Input";
 import Spacer from "../../components/Spacer";
 import useGlobalDataCache from "../../hooks/useGlobalDataCache";
 import { NeverImport } from "../../types/Category";
-import FormProps from "../../types/FormProps";
 import MemoViewModel from "../../types/MemoViewModel";
-import { useEffect } from "react";
 
-function MemoForm(props: FormProps<MemoViewModel>) {
-    const { onSubmit, onDelete, onCancel, values } = props;
-
+function MemoForm(props: EntityManagementFormProps<MemoViewModel>) {
     const {
-        formState: { errors },
-        handleSubmit,
-        reset,
-        setValue,
-        control,
-        watch,
-    } = useForm<MemoViewModel>();
-
-    useEffect(() => {
-        reset(values);
-    }, [values]);
+        form: {
+            formState: { errors },
+            setValue,
+            control,
+            watch,
+        },
+    } = props;
 
     const globalDataCache = useGlobalDataCache();
 
-    const catSelectorValue = values?.isImported
+    const catSelectorValue = watch("isImported")
         ? globalDataCache.getCategoryById(watch("categoryId"))
         : NeverImport;
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-                <h2>Edit Memo</h2>
+        <>
+            <Spacer height={24} />
 
-                <Spacer height={24} />
-
-                <FormGroup fieldName="Memo">
-                    <Input readOnly name="memo" value={values?.memo ?? ""} />
-                </FormGroup>
-                <FormGroup fieldName="Category" error={errors.categoryId}>
-                    <Controller
-                        control={control}
-                        name="categoryId"
-                        render={() => (
-                            <CategorySelector
-                                disabled={!values?.isImported}
-                                categories={globalDataCache.categories.value}
-                                onChange={(c) => setValue("categoryId", c?.id)}
-                                value={catSelectorValue}
-                            />
-                        )}
-                        rules={{
-                            required: true,
-                        }}
-                    />
-                </FormGroup>
-            </div>
-            <div>
-                <div>
-                    <Button type="button" onClick={onCancel}>
-                        Cancel
-                    </Button>
-                    <ButtonFill type="submit" disabled={!values?.isImported}>
-                        Submit
-                    </ButtonFill>
-                </div>
-                <div>
-                    <Button type="button" onClick={onDelete}>
-                        Delete
-                    </Button>
-                </div>
-            </div>
-        </form>
+            <FormGroup fieldName="Memo">
+                <Input readOnly name="memo" value={watch("memo")} />
+            </FormGroup>
+            <FormGroup fieldName="Category" error={errors.categoryId}>
+                <Controller
+                    control={control}
+                    name="categoryId"
+                    render={() => (
+                        <CategorySelector
+                            disabled={!watch("isImported")}
+                            categories={globalDataCache.categories.value}
+                            onChange={(c) => setValue("categoryId", c?.id)}
+                            value={catSelectorValue}
+                        />
+                    )}
+                    rules={{
+                        required: true,
+                    }}
+                />
+            </FormGroup>
+        </>
     );
 }
 
